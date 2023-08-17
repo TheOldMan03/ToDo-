df8d6324aac72cf3b21149a803bd6f3848ed6c1c
Joel Antony <joel.geoanto@gmail.com>
2023-08-17 20:16:22 +0530

    Removed the top buttons-Too ugly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Selector from './components/Selector';
 import AddTask from './components/AddTask';
 import TaskDesc from './components/TaskDesc';
 import { taskInfo } from './components/context/task_info';
@@ -17,13 +16,6 @@ function App() {
     <div className='main-div'> {/* this is the main div */}
 
       <div className='stuff-div'>
-
-        <div className='button-side'>
-          <Selector text="Reminders" />
-          <Selector text="To Do" />
-          <Selector text="Shopping List" />
-        </div>
-
 
         <taskInfo.Provider value={{ task, setTask, desc, setDesc,isTask,setIsTask,data,setData }}>
           <div className="inputfield">
